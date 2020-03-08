76a918cd86c7762b461db03f4603767fc6f39856
Emre Usta <emreustaa34@gmail.com>
2020-03-09 00:42:05 +0300

    Hw 1

---
+++ Homework 1/__init__.py
@@ -0,0 +1,18 @@
+text = "John is the son John second second son of John second is William second"
+dict = {}
+
+for word in text.split():
+    x = dict.setdefault(word)
+    if x is not None:
+        dict[word] = dict.get(word) + 1
+    else:
+        dict[word] = 1
+
+maxWord = max(dict, key=dict.get)
+print("Most Frequent word is : " , maxWord)
+
+dict = {}
+
+dict = {key: text.split().count(key) for key in text.split()}
+maxWord = max(dict, key=dict.get)
+print("Most Frequent word is : " + maxWord)
